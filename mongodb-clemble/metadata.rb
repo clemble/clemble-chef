5b9d8fddbc23344e1987a2f5ecda55cabc7006f0
Anton O <mavarazy@gmail.com>
2014-06-21 06:49:33 +0300

    Adding mongo recipes

---
+++ mongodb-clemble/metadata.rb
@@ -0,0 +1,8 @@
+
+name        "mongodb-clemble"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "MyOrg"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
