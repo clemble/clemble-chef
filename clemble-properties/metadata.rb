7943dbe6ebfb2c1c833738b3ed06d4a4cd4d0d45
Anton O <mavarazy@gmail.com>
2014-06-22 22:29:36 +0300

    Adding recipe for Clemble

---
+++ clemble-properties/metadata.rb
@@ -0,0 +1,6 @@
+
+name        "clemble-properties"
+description 'Example cookbook for Clemble properties'
+maintainer  "Clemble"
+license     "Apache 2.0"
+version     "1.0.0"
---
+++ clemble-properties/recipes/default.rb
@@ -0,0 +1,7 @@
+
+file "~/clemble/config/" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode "0755"
+  action :create
+end
